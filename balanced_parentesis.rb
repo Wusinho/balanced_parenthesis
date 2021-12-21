010796940e07c4ec96afa108f6c8266e22fb8f26
Heber Lazo <heberlazo@gmail.com>
2021-12-21 15:43:43 -0500

    aproach

--- balanced_parentesis.rb
+++ balanced_parentesis.rb
@@ -4,46 +4,87 @@
 #4. Es un mensaje con paréntesis balanceados seguido de otro mensaje con paréntesis balanceados
 #5. Es un emoticón feliz ":)" o uno triste ":("
 
-class Balanceado
+# class Balanceado
 
-  attr_accessor :string
-  attr_reader   :faces
+#   attr_accessor :string
+#   attr_reader   :faces
 
-  def initialize(string)
-    @string = arr.tr('a-zA-Z0-9', '')
-    @faces = [':)', ':(']
-  end
+#   def initialize(string)
+#     @string = string.tr('a-zA-Z0-9', '').split('')
+#     @faces = [':)', ':(']
+#     @open_par = 0
+#     @closed_par = 0
+#   end
 
-  def checkString
-    test_string = @string
-    counter = 0
-
-    loop do
-
-      do_puntos = test_string.index(':)')
-      close_par =  test_string.index(')')
-      open_par = nil
-      test_string
+#   def checkString
+#     test_string = @string
     
-      break unless close_par
+#     test_string.each do |ele|
+
+#       @open_par += 1 if ele == '('
+       
+#       break if ele != '('
+
+#     end
+#     puts @open_par
+
+#   end
 
 
-      counter += 1
-      break if counter == 10
-    end
-  end
+#   def removefaces(str)
+#     loop do
 
-
-  def removefaces
+#       do_puntos = test_string.index(':)')
+#       close_par =  test_string.index(')')
+#       open_par = nil
+#       test_string
     
+#       break unless close_par
+
+
+#       counter += 1
+#       break if counter == 10
+#     end
+#   end
+
+
+
+# end
+
+# balanceado = Balanceado.new('((:):):)))')
+# balanceado.checkString
+
+
+def balacend_parenthesis(string)
+  arr = string.tr('a-zA-Z0-9', '').split('')
+
+  counter = 0
+
+
+  loop do
+
+
+    
+
+
+
+
+
+
+
+    p counter
+    counter +=1 
+
+    break if counter == 10
   end
-
-
 
 end
 
-balanceado = Balanceado.new('((:):):)))')
-puts balanceado.arr
+
+
+
+# puts balacend_parenthesis('((:):):)') #balanceado
+# puts balacend_parenthesis('((:):):)))') #balanceado
 
 # puts balacend_parenthesis('') #balanceado
 # puts balacend_parenthesis('hola') #balanceado
@@ -53,7 +94,6 @@ puts balanceado.arr
 # puts balacend_parenthesis(':):)') #balanceado
 # puts balacend_parenthesis(":((" ) #desbalanceado
 # puts balacend_parenthesis("a (b (c (d) c) b) a :)") #balanceado
-# puts balacend_parenthesis('((:):):)') #balanceado
 # puts balacend_parenthesis(':)(:') #balanceado
 # puts balacend_parenthesis('(:::(:)')  #desbalanceado
 # puts balacend_parenthesis(')))(((') #desbalanceado
